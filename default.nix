bb7749e950d0847b7c5ce620cd8cf56b7476d5e5
Sam <30577766+Samasaur1@users.noreply.github.com>
2025-04-14 13:20:54 -0700

    update vendor hash

--- default.nix
+++ default.nix
@@ -6,7 +6,7 @@ buildGoModule {
 
   src = ./.;
 
-  vendorHash = "sha256-GjSrYT+PMPUn23VaXtd+DPS+zAcIQDCvNcVpL8rcN6U=";
+  vendorHash = "sha256-Fd5qslTNmFTpEaY37Yb0nRDSjGXJpr/D0Ux+6Kj4HT4=";
 
   meta.mainProgram = "cliff";
 }
--- go.mod
+++ go.mod
@@ -1,6 +1,7 @@
 module github.com/Samasaur1/cliff
 
-go 1.22.5
+go 1.23.0
+
 toolchain go1.24.1
 
 require (
