eba71be4fb4ff6a848acf387e9505323b7978fcc
Sam <30577766+Samasaur1@users.noreply.github.com>
2025-04-14 13:37:58 -0700

    flake.lock: update
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/ac35b104800bff9028425fec3b6e8a41de2bbfff?narHash=sha256-t4BBVpwG9B4hLgc6GUBuj3cjU7lP/PJfpTHuSqE%2Bcrk%3D' (2024-12-01)
      → 'github:NixOS/nixpkgs/63d95d78f073559d8372ce27472a89874532f2f5?narHash=sha256-VVa93wkzCQ/gGOXxvj5ASaPlu05sz9c3qrcsXYkL/DM%3D' (2025-04-14)

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Listen for HTTP requests over Tailscale and send APNs pushes";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs?rev=ac35b104800bff9028425fec3b6e8a41de2bbfff";
+    nixpkgs.url = "github:NixOS/nixpkgs";
   };
 
   outputs = { self, nixpkgs, ... }:
